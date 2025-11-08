7e643b79a42c13ed3fec9e100b1287bc6f103d27
Ramyagayatri-bandaru <142379816+Ramyagayatri-bandaru@users.noreply.github.com>
2025-11-08 12:12:30 +0530

    Update food.model.js

--- models/food.model.js
+++ models/food.model.js
@@ -1,10 +1,12 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
 const foodItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   quantity: { type: Number, required: true },
+  manufactureDate: { type: Date, required: true },
   expiryDate: { type: Date, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 }, { timestamps: true });
 
-export default mongoose.model('FoodItem', foodItemSchema);
+module.exports = mongoose.model("FoodItem", foodItemSchema);
+
